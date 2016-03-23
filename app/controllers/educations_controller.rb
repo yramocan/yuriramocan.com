a86f7cedba9c308100679158325347622337b18d
yramocan <yramocan@umich.edu>
2016-03-23 19:31:59 -0400

    Remove modify actions from controllers for deploy

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,56 +1,56 @@
 class EducationsController < ApplicationController
-  def edit
-    @education = Education.find(params[:id])
-  end
-
-  def new
-    @education = Education.new
-  end
-
-  def create
-    # Create a new instance of education with education_params
-    @education = Education.new(education_params)
-
-    if @education.save
-      # If education successfully saves, notify user and redirect to
-      # the education "show" page.
-      flash[:success] = "Successfully added education."
-      redirect_to root_path
-    else
-      # If education did not successfully save, alert user and send
-      # back to "new" page.
-      flash[:error] = "There was a problem adding that education."
-      render action: :new
-    end
-  end
-
-  def update
-    @education = Education.find(params[:id])
-    if @education.update_attributes(education_params)
-      # If education attributes successfully update, notify user and
-      # redirect to the education "show" page.
-      flash[:success] = "Successfully saved education."
-      redirect_to root_path
-    else
-      # If education attributes did not successfully update, alert
-      # user and send back to the "edit" page.
-      flash[:error] = "That education could not be saved."
-      render action: :edit
-    end
-  end
-
-  def destroy
-    @education = Education.find(params[:id])
-    # If education is successfully deleted, notify user.
-    # Otherwise, alert the user of the error.
-    if @education.destroy
-      flash[:notice] = "Education was deleted."
-    else
-      flash[:error] = "Education could not be deleted."
-    end
-
-    redirect_to education_path
-  end
+  # def edit
+  #   @education = Education.find(params[:id])
+  # end
+  #
+  # def new
+  #   @education = Education.new
+  # end
+  #
+  # def create
+  #   # Create a new instance of education with education_params
+  #   @education = Education.new(education_params)
+  #
+  #   if @education.save
+  #     # If education successfully saves, notify user and redirect to
+  #     # the education "show" page.
+  #     flash[:success] = "Successfully added education."
+  #     redirect_to root_path
+  #   else
+  #     # If education did not successfully save, alert user and send
+  #     # back to "new" page.
+  #     flash[:error] = "There was a problem adding that education."
+  #     render action: :new
+  #   end
+  # end
+  #
+  # def update
+  #   @education = Education.find(params[:id])
+  #   if @education.update_attributes(education_params)
+  #     # If education attributes successfully update, notify user and
+  #     # redirect to the education "show" page.
+  #     flash[:success] = "Successfully saved education."
+  #     redirect_to root_path
+  #   else
+  #     # If education attributes did not successfully update, alert
+  #     # user and send back to the "edit" page.
+  #     flash[:error] = "That education could not be saved."
+  #     render action: :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @education = Education.find(params[:id])
+  #   # If education is successfully deleted, notify user.
+  #   # Otherwise, alert the user of the error.
+  #   if @education.destroy
+  #     flash[:notice] = "Education was deleted."
+  #   else
+  #     flash[:error] = "Education could not be deleted."
+  #   end
+  #
+  #   redirect_to education_path
+  # end
 
   private
   def education_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'educations/edit'
-
   get 'educations/new'
 
   resources :projects
